fead53bd9503083e447cf87dcebc629edae4852c
MacUser <iurii.gubanov@macbook-pro-magora.local>
2016-09-11 12:23:40 +0300

    Initial Commit

---
+++ test-imdb/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test-imdb
+//
+//  Created by Iurii Gubanov on 11.09.16.
+//  Copyright © 2016 Iurii Gubanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test-imdb/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test-imdb
+//
+//  Created by Iurii Gubanov on 11.09.16.
+//  Copyright © 2016 Iurii Gubanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
